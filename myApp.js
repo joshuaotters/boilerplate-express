847865f6de698c2e8d63e5753d25623f5d84358c
joshuaotters <joshuasemwanga@gmail.com>
2023-11-13 00:21:52 +0300

    Updated to them.

--- myApp.js
+++ myApp.js
@@ -1,8 +1,18 @@
 let express = require('express');
 let app = express();
+
+app.use(express.static(__dirname + "/public"));
+// Assets at the /public route
+app.use("/public", express.static(__dirname + "/public"));
+
+
 app.get("/", (req, res) => {
     res.sendFile(absolutePath = __dirname + '/views/index.html');
 })
+app.get("/public", (req, res) => {
+    
+})
+
 
 
 
