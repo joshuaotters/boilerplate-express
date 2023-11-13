379656c032d3973f89bb3a3968dc7e14b1f2efb3
joshuaotters <joshuasemwanga@gmail.com>
2023-11-13 15:41:40 +0300

    added json route

--- myApp.js
+++ myApp.js
@@ -10,6 +10,9 @@ app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (req, res) => {
     res.sendFile(absolutePath = __dirname + '/views/index.html');
 });
+app.get("/json", (req, res) => {
+    res.json({"message": "Hello json"});
+})
 
 
 
