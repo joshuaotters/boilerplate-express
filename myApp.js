44ebfdc4a5e8b8e0d086cb546206d077271ead0d
joshuaotters <joshuasemwanga@gmail.com>
2023-11-13 22:10:16 +0300

    Finally nailed it

--- myApp.js
+++ myApp.js
@@ -9,25 +9,21 @@ let app = express();
 
 // Assets at the /public route
 app.use("/public", express.static(__dirname + "/public"));
-var response = "Hello World".toUpperCase(); // now becomes "HELLO WORLD"
-
-
-
-
+var response = "Hello Json".toUpperCase(); // now becomes "HELLO WORLD"
 
 app.get("/", (req, res) => {
     res.sendFile(absolutePath = __dirname + '/views/index.html');
 });
 app.get("/json", (req, res) => {
     //Do it the FCC way
-    if (process.env.MESSAGE_STYLE === "uppercase") {
+    if (process.env.MESSAGE_STYLE == "uppercase") {
         response = "Hello json".toUpperCase();
         res.json({"message" : response});
-        console.log("Response: ", response + "Message Style: ", process.env.VAR_NAME); 
+        console.log("Response: ", response + " "+ "Message Style: ", process.env.MESSAGE_STYLE); 
       } else {
         response = "Hello json";
         res.json({"message" : response}); 
-        console.log("Response: ", response + "Message Style: ", process.env.VAR_NAME );  
+        console.log("Response: ", response + " "+ "Message Style: ", process.env.MESSAGE_STYLE);  
       }
       
 });
