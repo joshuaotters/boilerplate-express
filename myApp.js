6404b373e316a65d5473e1a98a35810b4af00405
joshuaotters <joshuasemwanga@gmail.com>
2023-11-13 21:01:29 +0300

    debug changes for .env query

--- myApp.js
+++ myApp.js
@@ -9,7 +9,7 @@ let app = express();
 
 // Assets at the /public route
 app.use("/public", express.static(__dirname + "/public"));
-var response = "Hello Json";
+var response = "Hello World".toUpperCase(); // now becomes "HELLO WORLD"
 
 
 
@@ -19,15 +19,12 @@ app.get("/", (req, res) => {
     res.sendFile(absolutePath = __dirname + '/views/index.html');
 });
 app.get("/json", (req, res) => {
-    if(process.env.MESSAGE_STYLE === "uppercase"){
-        res.json({"message": response.toUpperCase()});
-        console.log(process.env.MESSAGE_STYLE.toUpperCase());
-    } else {
-        res.json({"message": "Hello json"});
-        console.log(process.env.MESSAGE_STYLE.toUpperCase());
-    }
-    
-})
+    if (process.env.VAR_NAME === "allCaps") {
+        response = "Hello World".toUpperCase();
+      } else {
+        response = "Hello World";
+      }  
+});
 
 
 
