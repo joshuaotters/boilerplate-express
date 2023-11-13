62e7f2451fcc3edff2d866ff2f50ae9f24ec5fcd
joshuaotters <joshuasemwanga@gmail.com>
2023-11-13 22:20:14 +0300

    fix .env variables access

--- myApp.js
+++ myApp.js
@@ -4,9 +4,6 @@ require('dotenv').config();
 let express = require('express');
 let app = express();
 
-
-//app.use(express.static(__dirname + "/public"));
-
 // Assets at the /public route
 app.use("/public", express.static(__dirname + "/public"));
 var response = "Hello Json".toUpperCase(); // now becomes "HELLO WORLD"
@@ -14,7 +11,7 @@ var response = "Hello Json".toUpperCase(); // now becomes "HELLO WORLD"
 app.get("/", (req, res) => {
     res.sendFile(absolutePath = __dirname + '/views/index.html');
 });
-app.get("/json", (req, res) => {
+app.get("/json", (request, response) => {
     //Do it the FCC way
     if (process.env.MESSAGE_STYLE == "uppercase") {
         response = "Hello json".toUpperCase();
