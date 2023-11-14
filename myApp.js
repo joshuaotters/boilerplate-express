984b8ea6831d1d8066f7e3033b5734314b44374b
joshuaotters <joshuasemwanga@gmail.com>
2023-11-14 18:56:15 +0300

    Changed middleware fx to arrow fx

--- myApp.js
+++ myApp.js
@@ -9,7 +9,7 @@ let app = express();
 app.use("/public", express.static(__dirname + "/public"));
 
 //Middleware to intercept all GET requests
-app.use(function middleware (req, res, next)  {
+app.use((req, res, next) => {
   console.log(req.method, + " " + req.path + " - " + req.ip);
   next();
 });
