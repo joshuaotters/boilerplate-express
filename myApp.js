782b4e8c0695576d3240cfd890b18cda36a7dd3b
joshuaotters <joshuasemwanga@gmail.com>
2023-11-15 00:08:37 +0300

    edit first fx in middleware

--- myApp.js
+++ myApp.js
@@ -16,6 +16,7 @@ app.use((req, res, next) => {
 
 app.get("/now", (req, res, next) => {
   req.time = new Date().toString();
+  next();
 }, (req, res) => {
   res.send({"time": req.time});
   next();
