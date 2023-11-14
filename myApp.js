c4574e7db5b7fbb5f6f7707accf58e744de5f781
joshuaotters <joshuasemwanga@gmail.com>
2023-11-14 20:07:58 +0300

    added /get middleware

--- myApp.js
+++ myApp.js
@@ -10,8 +10,14 @@ app.use("/public", express.static(__dirname + "/public"));
 
 //Middleware to intercept all GET requests
 app.use((req, res, next) => {
-  //console.log(req.method, + " " + req.path + " - " + req.ip);
   console.log(`${req.method} ${req.path} - ${req.ip}`);
+  next();
+});
+
+app.get("/get", (req, res, next) => {
+  req.time = new Date().toString();
+  res.send({"time": req.time});
+  console.log(`${req.time}`);
   next();
 });
 
