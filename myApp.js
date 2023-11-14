16bcff907579709057e27de2f978f6b5e2991fd2
joshuaotters <joshuasemwanga@gmail.com>
2023-11-14 15:26:30 +0300

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -8,7 +8,7 @@ let app = express();
 // Assets at the /public route
 app.use("/public", express.static(__dirname + "/public"));
 //Middleware to intercept all GET requests
-app.use( (req, res, next) => {
+app.use((req, res, next) => {
   console.log(req.method, + " " + req.path, " - " + req.ip);
   next();
 });
